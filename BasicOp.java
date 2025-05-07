df437ac109f0ccfe7345e09d779d97475ce48e8e
snehakittur <snehakittur17@gmail.com>
2025-05-07 16:01:40 +0530

    save

--- BasicOp.java
+++ BasicOp.java
@@ -1,12 +1,12 @@
 public class BasicOp {
     public static void main(String[] args) {
-    int a = 15;
-    int b = 5;
-    int sum = a + b;
-    int diff = a - b;
-    int prod = a * b;
-    System.out.println("Addition: " + sum);
-    System.out.println("Subtraction:" + diff);
-    System.out.println("Multiplication: " + prod);
+        int a = 15;
+        int b = 3; 
+    if (b != 0) {
+        int div = a / b;
+        System.out.println("Division: " + div);
+    } else {
+        System.out.println("Error: Cannot divide by zero.");
     }
-}
+    }
+    }
